6b1a6cb1dce911103cdef5547ee6b040f331a298
Sridesh Fernando <srideshhimalka@gmail.com>
2026-03-12 22:12:38 +0530

    minor changes

--- app/adaptive_testing_module/config.py
+++ app/adaptive_testing_module/config.py
@@ -140,9 +140,9 @@ MAX_ITEMS_TOTAL: int = 20
 P_CONFIDENT: float = 0.75
 ENTROPY_THRESHOLD: float = 0.70
 MIN_INFO_GAIN: float = 0.01
-RISK_SCORE_HIGH: float = 0.75
+RISK_SCORE_HIGH: float = 0.50
 # RISK_SCORE_MODERATE: float = 0.40
-RISK_SCORE_MODERATE = 0.60
+RISK_SCORE_MODERATE = 0.35
 
 
 # -------------------------------------------------
